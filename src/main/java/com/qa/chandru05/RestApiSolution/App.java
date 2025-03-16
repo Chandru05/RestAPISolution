ff842a84545ab58cf6e80b6fd939eae4b3dedc71
Chandru Karunanithi <ckarunanithi@opentext.com>
2025-03-16 23:34:29 +0530

    Initial Commit

---
+++ src/main/java/com/qa/chandru05/RestApiSolution/App.java
@@ -0,0 +1,13 @@
+package com.qa.chandru05.RestApiSolution;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
